04eb900c478f4d6e351fe9f6eec7d0164a442597
Don <donabrams@gmail.com>
2012-08-21 10:45:59 -0400

    remove made with heart. Just doesn't look right

--- wp-content/themes/responsive/footer.php
+++ wp-content/themes/responsive/footer.php
@@ -101,7 +101,7 @@ if ( !defined('ABSPATH')) exit;
         	<a href="http://www.battelle.org/"><img src="http://stemx.us/wp-content/uploads/2012/06/Battelle-logo.png"></a>
         </div>
         <div class="copyright">
-          &copy; 2012<a href="http://www.stemx.us/wp-login.php" title="STEMx"> Battelle - all rights reserved.</a> Student-<a href="http://madewithloveinbaltimore.org">Made with &hearts; in Baltimore</a></p>
+          &copy; 2012<a href="http://www.stemx.us/wp-login.php" title="STEMx"> Battelle - all rights reserved.</a>
         </div><!-- end of .copyright -->
         
         </div>
