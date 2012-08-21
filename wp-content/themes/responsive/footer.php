8530ea716c27e13701b86482aadbe473d2cbf2ab
Don <donabrams@gmail.com>
2012-08-21 10:10:42 -0400

    add student-made with heart in baltimore

--- wp-content/themes/responsive/footer.php
+++ wp-content/themes/responsive/footer.php
@@ -103,8 +103,9 @@ if ( !defined('ABSPATH')) exit;
         <div class="copyright">
           &copy; 2012<a href="http://www.stemx.us/wp-login.php" title="STEMx"> Battelle - all rights reserved</a>
         </div><!-- end of .copyright -->
-        </div>
         
+        </div>
+        <p>Student-<a href="http://madewithloveinbaltimore.org">Made with &hearts; in Baltimore</a></p>
         
     </div><!-- end of col-940 -->
     </div><!-- end #footer-wrapper -->
