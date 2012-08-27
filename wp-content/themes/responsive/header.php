6ab5005a2c7132add809a6292b7dd77ae7344fdd
Don <donabrams@gmail.com>
2012-08-27 11:43:00 -0400

    make page zoomable as per jean's catch

--- wp-content/themes/responsive/header.php
+++ wp-content/themes/responsive/header.php
@@ -26,7 +26,7 @@ if ( !defined('ABSPATH')) exit;
 <head>
 
 <meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 <title><?php wp_title('&#124;', true, 'right'); ?><?php bloginfo('name'); ?></title>
